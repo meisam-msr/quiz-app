65f8bdb2bd060fb83ac3bfcf6d4308324bb98016
meisam <maghsoodiradmeisam12@gmail.com>
2022-07-04 18:43:43 +0430

    add finalScore page

--- src/pages/FinalScore.js
+++ src/pages/FinalScore.js
@@ -1,5 +1,29 @@
+import { Box } from "@mui/system";
+import { Typography, Button } from "@mui/material";
+import { useSelector, useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { changeAmount, changeScore } from "../redux/questionSlice";
 const FinalScore = () => {
-  return <div>FinalScore</div>;
+  const { score } = useSelector((state) => state.questions);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const backToHome = () => {
+    dispatch(changeScore(0));
+    dispatch(changeAmount(50));
+    navigate("/");
+  };
+
+  return (
+    <Box mt={30}>
+      <Typography variant="h3" fontWeight="bold" mb={3}>
+        Final Score : {score}
+      </Typography>
+      <Button onClick={backToHome} variant="outlined">
+        Back to Home
+      </Button>
+    </Box>
+  );
 };
 
 export default FinalScore;
